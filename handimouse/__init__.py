83f5b6824bd10848870bc2ace1181a3f803f7650
Ali Nazar <muhammadali2005111@gmail.com>
2026-06-01 16:17:05 +0500

    feat: export all modular layer entrypoints cleanly in root package init

--- handimouse/__init__.py
+++ handimouse/__init__.py
@@ -2,4 +2,27 @@
 HandiMouse: A real-time computer vision-based virtual mouse control system.
 """
 
+from handimouse.input import CameraStream
+from handimouse.vision import HandTracker, OneEuroFilter
+from handimouse.intelligence import GestureEngine
+from handimouse.control import MouseController, MouseControllerFactory
+from handimouse.safety import BoundsValidator, SafetyInterlock
+from handimouse.config import AppSettings, ConfigurationManager
+from handimouse.monitoring import setup_logger
+
 __version__ = "0.1.0"
+
+__all__ = [
+    "CameraStream",
+    "HandTracker",
+    "OneEuroFilter",
+    "GestureEngine",
+    "MouseController",
+    "MouseControllerFactory",
+    "BoundsValidator",
+    "SafetyInterlock",
+    "AppSettings",
+    "ConfigurationManager",
+    "setup_logger"
+]
+
